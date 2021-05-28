3b2e1cd90b3e66f0f1321f326e5c4d8a7a400f41
samnix-p <samnix.mov@gmail.com>
2021-05-28 05:25:39 +0300

    fixup! GW-56 add ChapterResourceTest

--- back/src/test/resources/scripts/content_test_data.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO chapters (id, name) VALUES (1, 'chapter 1');
-INSERT INTO chapters (id, name) VALUES (2, 'chapter 2');
-
-INSERT INTO paragraphs (id, name, chapter_id) VALUES (1, 'par 1', 1);
-INSERT INTO paragraphs (id, name, chapter_id) VALUES (2, 'par 2', 1);
-INSERT INTO paragraphs (id, name, chapter_id) VALUES (3, 'par 1', 2);
