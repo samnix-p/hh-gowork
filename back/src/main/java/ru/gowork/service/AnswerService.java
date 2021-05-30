9fc016c8252a13feef729804be488f9b67fb6fd5
PavelDubrov <n1sh4dow@gmail.com>
2021-05-30 13:20:13 +0300

    final fixes

--- back/src/main/java/ru/gowork/service/AnswerService.java
+++ back/src/main/java/ru/gowork/service/AnswerService.java
@@ -2,7 +2,6 @@ package ru.gowork.service;
 
 import ru.gowork.dao.UserAnswerDao;
 import ru.gowork.dao.UserDao;
-import ru.gowork.entity.Step;
 import ru.gowork.entity.User;
 import ru.gowork.entity.UserAnswer;
 
@@ -19,16 +18,21 @@ public class AnswerService {
         this.userDao = userDao;
     }
 
-    public Optional<Object> saveAnswerGetExplanation(String userEmail, String answer) {
-        Optional<User> userOptional = userDao.getUserByEmail(userEmail);
-        if (userOptional.isPresent()) {
-            User user = userOptional.get();
-            Step step = user.getCurrentStep();
-            UserAnswer userAnswer = new UserAnswer(user, step, answer);
-            userAnswerDao.saveAnswer(userAnswer);
-            return Optional.of(step.getAnswersExplanations());
-        }
-        return Optional.empty();
-    }
+//    public Optional<Object> saveAnswerGetExplanation(String userEmail, String answer) {
+//        Optional<User> userOptional = userDao.getUserByEmail(userEmail);
+//        if (userOptional.isPresent()) {
+//            User user = userOptional.get();
+//            Step step = user.getCurrentStep();
+//            UserAnswer userAnswer = new UserAnswer(user, step, answer);
+//            userAnswerDao.saveAnswer(userAnswer);
+//            return Optional.of(step.getAnswersExplanations());
+//        }
+//        return Optional.empty();
 
+
+    public Optional<Object> saveAnswerGetExplanation(String userEmail, String answer) {
+        Optional<User> userOpt = userDao.getUserByEmail(userEmail);
+        userOpt.ifPresent(user -> userAnswerDao.saveAnswer(new UserAnswer(user, user.getCurrentStep(), answer)));
+        return userOpt.map(user -> user.getCurrentStep().getAnswersExplanations());
+        }
 }
